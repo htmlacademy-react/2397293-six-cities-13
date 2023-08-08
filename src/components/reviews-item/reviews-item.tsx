6c86c1355a42c9fd5696d47e74c569d63cf5fef5
andreyishchenko <andreyishchenko@sprut.ru>
2023-08-08 13:48:13 +0300

    Форматирует дату на ревью лист

--- src/components/reviews-item/reviews-item.tsx
+++ src/components/reviews-item/reviews-item.tsx
@@ -5,6 +5,19 @@ interface ReviewsItemProps {
 }
 
 function ReviewsItem({ reviewData }: ReviewsItemProps) {
+	const dateConvert = (value: string) => {
+		let createBody;
+		if (value) {
+			const date = new Date(value);
+			const day = String(date.getDate()).padStart(2, '0');
+			const month = String(date.getMonth() + 1).padStart(2, '0');
+			const year = date.getFullYear();
+			const data = `${year}-${month}-${day}`;
+			createBody = data;
+		}
+		return createBody;
+	};
+
 	return (
 		<li className="reviews__item">
 			<div className="reviews__user user">
@@ -28,7 +41,7 @@ function ReviewsItem({ reviewData }: ReviewsItemProps) {
 				</div>
 				<p className="reviews__text">{reviewData.comment}</p>
 				<time className="reviews__time" dateTime="2019-04-24">
-					{reviewData.date}
+					{dateConvert(reviewData.date)}
 				</time>
 			</div>
 		</li>
